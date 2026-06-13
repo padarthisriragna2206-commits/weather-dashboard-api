df4988af5b7d0e6a122975b4abb8522815100833
padarthisriragna2206-commits <padarthisriragna2206@gmail.com>
2026-06-13 11:07:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const API_KEY = "YOUR_API_KEY";
+
+const btn = document.getElementById("searchBtn");
+
+btn.addEventListener("click", async () => {
+    const city = document.getElementById("cityInput").value;
+
+    try {
+        const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        );
+
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+
+        const data = await response.json();
+
+        document.getElementById("city").textContent = data.name;
+        document.getElementById("temp").textContent = data.main.temp;
+        document.getElementById("humidity").textContent = data.main.humidity;
+        document.getElementById("wind").textContent = data.wind.speed;
+
+        document.getElementById("error").textContent = "";
+
+    } catch (error) {
+        document.getElementById("error").textContent = error.message;
+    }
+});
